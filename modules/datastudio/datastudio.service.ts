224cf0d9d8a4f8c7da67c87478fda38991d90f3a
franz <franzns11@gmail.com>
2022-10-25 17:49:59 +0200

    first go on datafeed for datastudio

---
+++ modules/datastudio/datastudio.service.ts
@@ -0,0 +1,234 @@
+import { prisma } from '../../prisma/prisma-client';
+import { google } from 'googleapis';
+import { env } from '../../app/env';
+import { DeploymentEnv, networkConfig } from '../config/network-config';
+import moment from 'moment-timezone';
+import { JWT } from 'google-auth-library';
+import { SecretsManagerClient, GetSecretValueCommand } from '@aws-sdk/client-secrets-manager';
+
+export class DatastudioService {
+    public async feedPoolData() {
+        const secretManagerClient = new SecretsManagerClient({ region: env.AWS_REGION });
+
+        let privateKey = '';
+        try {
+            const response = await secretManagerClient.send(
+                new GetSecretValueCommand({ SecretId: 'backend-v2-datafeed-privatekey' }),
+            );
+            if (response.SecretString) {
+                privateKey = response.SecretString;
+                console.log(privateKey);
+            }
+        } catch (error) {
+            throw Error(`Error getting secret from AWS: ${error}`);
+        }
+
+        const jwtClient = new google.auth.JWT(
+            networkConfig.datastudio[env.DEPLOYMENT_ENV as DeploymentEnv].user,
+            undefined,
+            privateKey,
+            'https://www.googleapis.com/auth/spreadsheets',
+        );
+        await jwtClient.authorize(function (err, result) {
+            if (err) {
+                console.log(`Error authorizing: ${err}`);
+                return;
+            }
+        });
+
+        const sheets = google.sheets({ version: 'v4' });
+
+        const range = `${networkConfig.datastudio[env.DEPLOYMENT_ENV as DeploymentEnv].databaseTabName}!B2:B`;
+        let result;
+        try {
+            result = await sheets.spreadsheets.values.get({
+                auth: jwtClient,
+                spreadsheetId: networkConfig.datastudio[env.DEPLOYMENT_ENV as DeploymentEnv].sheetId,
+                range: range,
+            });
+        } catch (e) {
+            throw Error(`Error reading values: ${e}`);
+        }
+
+        let lastRun = moment.tz('GMT').startOf('day').subtract(1, 'day').unix();
+        if (result.data.values) {
+            lastRun = result.data.values[result.data.values.length - 1][0];
+        }
+
+        if (lastRun > moment.tz('GMT').subtract(1, 'day').unix()) {
+            // 24 hours did not pass since the last run
+            return;
+        }
+
+        const today = moment.tz('GMT').startOf('day');
+        const allPoolDataRows: string[][] = [];
+        const allPoolTypeRows: string[][] = [];
+        const allPoolCompositionRows: string[][] = [];
+        const pools = await prisma.prismaPool.findMany({
+            where: {
+                dynamicData: {
+                    totalLiquidity: { gte: 5000 },
+                },
+            },
+            include: {
+                dynamicData: true,
+                tokens: {
+                    include: {
+                        dynamicData: true,
+                    },
+                },
+                allTokens: {
+                    include: {
+                        token: true,
+                    },
+                },
+            },
+        });
+        for (const pool of pools) {
+            let sharesChange = `0`;
+            let tvlChange = `0`;
+            let lpSwapFee = `0`;
+            let protocolSwapFee = `0`;
+            let dailySwaps = `0`;
+
+            const yesterday = moment.tz('GMT').startOf('day').subtract(1, 'day').unix();
+
+            const snapshotFrom24HrsAgo = await prisma.prismaPoolSnapshot.findFirst({
+                where: {
+                    poolId: pool.id,
+                    timestamp: { lte: yesterday },
+                },
+            });
+
+            if (pool.dynamicData) {
+                sharesChange = `${
+                    parseFloat(pool.dynamicData.totalShares24hAgo) - parseFloat(pool.dynamicData.totalShares)
+                }`;
+                tvlChange = `${pool.dynamicData.totalLiquidity24hAgo - pool.dynamicData.totalLiquidity}`;
+                lpSwapFee = `${pool.dynamicData.fees24h * (1 - networkConfig.balancer.swapProtocolFeePercentage)}`;
+                protocolSwapFee = `${pool.dynamicData.fees24h * networkConfig.balancer.swapProtocolFeePercentage}`;
+
+                if (snapshotFrom24HrsAgo) {
+                    dailySwaps = `${pool.dynamicData.swapsCount - snapshotFrom24HrsAgo.swapsCount}`;
+                }
+            }
+
+            const poolDataRow: string[] = [];
+
+            const swapFee = pool.dynamicData?.swapFee || `0`;
+
+            poolDataRow.push(
+                today.format('DD MMM YYYY'),
+                `${today.unix()}`,
+                pool.address,
+                pool.type,
+                pool.name,
+                swapFee,
+                pool.dynamicData?.swapsCount ? `${pool.dynamicData.swapsCount}` : `0`,
+                pool.symbol,
+                pool.dynamicData?.totalLiquidity ? `${pool.dynamicData.totalLiquidity}` : `0`,
+                pool.dynamicData?.totalShares ? `${pool.dynamicData.totalShares}` : `0`,
+                pool.dynamicData?.lifetimeSwapFees ? `${pool.dynamicData.lifetimeSwapFees}` : `0`,
+                pool.dynamicData?.lifetimeVolume ? `${pool.dynamicData.lifetimeVolume}` : `0`,
+                pool.dynamicData?.volume24h ? `${pool.dynamicData.volume24h}` : `0`,
+                pool.dynamicData?.fees24h ? `${pool.dynamicData.fees24h}` : `0`,
+                sharesChange,
+                tvlChange,
+                dailySwaps,
+                `1`,
+                lpSwapFee,
+                protocolSwapFee,
+            );
+            allPoolDataRows.push(poolDataRow);
+
+            const poolTypeRow: string[] = [];
+            poolTypeRow.push(pool.address, pool.name, pool.id, pool.type, swapFee, pool.symbol);
+            allPoolTypeRows.push(poolTypeRow);
+
+            const allTokens = pool.allTokens.map((token) => {
+                const poolToken = pool.tokens.find((poolToken) => poolToken.address === token.token.address);
+
+                return {
+                    ...token.token,
+                    weight: poolToken?.dynamicData?.weight,
+                };
+            });
+
+            for (const token of allTokens) {
+                const poolCompositionRow: string[] = [];
+                poolCompositionRow.push(
+                    token.address,
+                    token.name,
+                    pool.id,
+                    pool.address,
+                    token.symbol,
+                    token.weight ? token.weight : `0`,
+                    pool.name,
+                );
+                allPoolCompositionRows.push(poolCompositionRow);
+            }
+        }
+
+        this.appendDataInSheet(
+            networkConfig.datastudio[env.DEPLOYMENT_ENV as DeploymentEnv].databaseTabName,
+            'A1:T1',
+            allPoolDataRows,
+            jwtClient,
+        );
+        this.updateDataInSheet(
+            networkConfig.datastudio[env.DEPLOYMENT_ENV as DeploymentEnv].poolTypesTabName,
+            `A2:F${allPoolTypeRows.length + 1}`,
+            allPoolTypeRows,
+            jwtClient,
+        );
+
+        this.updateDataInSheet(
+            networkConfig.datastudio[env.DEPLOYMENT_ENV as DeploymentEnv].compositionTabName,
+            `A2:G${allPoolCompositionRows.length + 1}`,
+            allPoolCompositionRows,
+            jwtClient,
+        );
+    }
+
+    private async updateDataInSheet(tabName: string, rowRange: string, rows: string[][], jwtClient: JWT) {
+        const sheets = google.sheets({ version: 'v4' });
+
+        try {
+            await sheets.spreadsheets.values.update({
+                auth: jwtClient,
+                spreadsheetId: networkConfig.datastudio[env.DEPLOYMENT_ENV as DeploymentEnv].sheetId,
+                range: `${tabName}!${rowRange}`,
+                valueInputOption: 'USER_ENTERED',
+                requestBody: {
+                    majorDimension: 'ROWS',
+                    range: `${tabName}!${rowRange}`,
+                    values: rows,
+                },
+            });
+        } catch (e) {
+            throw Error(`Error updating values: ${e}`);
+        }
+    }
+
+    private async appendDataInSheet(tabName: string, rowRange: string, rows: string[][], jwtClient: JWT) {
+        const sheets = google.sheets({ version: 'v4' });
+
+        try {
+            await sheets.spreadsheets.values.append({
+                auth: jwtClient,
+                spreadsheetId: networkConfig.datastudio[env.DEPLOYMENT_ENV as DeploymentEnv].sheetId,
+                range: `${tabName}!${rowRange}`,
+                valueInputOption: 'USER_ENTERED',
+                requestBody: {
+                    majorDimension: 'ROWS',
+                    range: `${tabName}!${rowRange}`,
+                    values: rows,
+                },
+            });
+        } catch (e) {
+            throw Error(`Error appending values: ${e}`);
+        }
+    }
+}
+
+export const datastudioService = new DatastudioService();
