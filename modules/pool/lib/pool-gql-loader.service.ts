edbbf4a66d133f5ce279a01281822c6aa0cd54dc
0xSkly <skly.beets@protonmail.com>
2022-11-14 16:19:47 +0100

    support apr ranges

--- modules/pool/lib/pool-gql-loader.service.ts
+++ modules/pool/lib/pool-gql-loader.service.ts
@@ -417,11 +417,72 @@ export class PoolGqlLoaderService {
             fees24hAth,
             fees24hAtlTimestamp,
         } = pool.dynamicData!;
-        const aprItems = pool.aprItems?.filter((item) => item.apr > 0) || [];
+        const aprItems = pool.aprItems?.filter((item) => item.apr > 0 || (item.range?.min ?? 0 > 0)) || [];
         const swapAprItems = aprItems.filter((item) => item.type == 'SWAP_FEE');
-        const nativeRewardAprItems = aprItems.filter((item) => item.type === 'NATIVE_REWARD');
-        const thirdPartyRewardAprItems = aprItems.filter((item) => item.type === 'THIRD_PARTY_REWARD');
+
+        // swap apr cannot have a range, so we can already sum it up
         const aprItemsWithNoGroup = aprItems.filter((item) => !item.group);
+
+        const hasAprRange = !!aprItems.find((item) => item.range);
+        let totalApr: string;
+        let swapApr: string;
+        let nativeRewardApr: string;
+        let thirdPartyApr: string;
+
+        let hasRewardApr = false;
+
+        if (hasAprRange) {
+            // if an apr item has a range, the apr field is 0, so we can sum all up and have the base apr
+            let swapFeeApr = 0;
+            let minTotalApr = 0;
+            let maxTotalApr = 0;
+            let minNativeRewardApr = 0;
+            let maxNativeRewardApr = 0;
+            let minThirdPartyApr = 0;
+            let maxThirdPartyApr = 0;
+            for (let aprItem of aprItems) {
+                let minApr: number;
+                let maxApr: number;
+                if (aprItem.range) {
+                    minApr = aprItem.range.min;
+                    maxApr = aprItem.range.max;
+                } else {
+                    minApr = aprItem.apr;
+                    maxApr = aprItem.apr;
+                }
+                minTotalApr += minApr;
+                maxTotalApr += maxApr;
+
+                switch (aprItem.type) {
+                    case 'NATIVE_REWARD': {
+                        minNativeRewardApr += minApr;
+                        maxNativeRewardApr += maxApr;
+                        break;
+                    }
+                    case 'THIRD_PARTY_REWARD': {
+                        minThirdPartyApr += minApr;
+                        maxThirdPartyApr += maxApr;
+                    }
+                    case 'SWAP_FEE': {
+                        swapFeeApr += maxApr;
+                        break;
+                    }
+                }
+            }
+            swapApr = `${swapFeeApr}`;
+            totalApr = `${minTotalApr} - ${maxTotalApr}`;
+            nativeRewardApr = `${minNativeRewardApr} - ${maxNativeRewardApr}`;
+            thirdPartyApr = `${minThirdPartyApr} - ${maxThirdPartyApr}`;
+            hasRewardApr = minNativeRewardApr > 0 || minThirdPartyApr > 0;
+        } else {
+            const nativeRewardAprItems = aprItems.filter((item) => item.type === 'NATIVE_REWARD');
+            const thirdPartyRewardAprItems = aprItems.filter((item) => item.type === 'THIRD_PARTY_REWARD');
+            totalApr = `${_.sumBy(aprItems, 'apr')}`;
+            swapApr = `${_.sumBy(swapAprItems, 'apr')}`;
+            nativeRewardApr = `${_.sumBy(nativeRewardAprItems, 'apr')}`;
+            thirdPartyApr = `${_.sumBy(thirdPartyRewardAprItems, 'apr')}`;
+            hasRewardApr = nativeRewardAprItems.length > 0 || thirdPartyRewardAprItems.length > 0;
+        }
 
         const grouped = _.groupBy(
             aprItems.filter((item) => item.group),
@@ -458,18 +519,25 @@ export class PoolGqlLoaderService {
             volume24hAthTimestamp,
             volume24hAtlTimestamp,
             apr: {
-                total: `${_.sumBy(aprItems, 'apr')}`,
-                swapApr: `${_.sumBy(swapAprItems, 'apr')}`,
-                nativeRewardApr: `${_.sumBy(nativeRewardAprItems, 'apr')}`,
-                thirdPartyApr: `${_.sumBy(thirdPartyRewardAprItems, 'apr')}`,
+                total: totalApr,
+                swapApr,
+                nativeRewardApr,
+                thirdPartyApr,
                 items: [
-                    ...aprItemsWithNoGroup.map((item) => ({
-                        ...item,
-                        apr: `${item.apr}`,
-                        subItems: [],
-                    })),
+                    ...aprItemsWithNoGroup.map((item) => {
+                        let apr = `${item.apr}`;
+                        if (item.range) {
+                            apr = `${item.range.min} - ${item.range.max}`;
+                        }
+                        return {
+                            ...item,
+                            apr,
+                            subItems: [],
+                        };
+                    }),
                     ..._.map(grouped, (items, group) => {
                         const subItems = items.map((item) => ({ ...item, apr: `${item.apr}` }));
+                        // todo: might need to support apr ranges as well at some point
                         const apr = _.sumBy(items, 'apr');
                         let title = '';
 
@@ -492,7 +560,7 @@ export class PoolGqlLoaderService {
                         };
                     }),
                 ],
-                hasRewardApr: nativeRewardAprItems.length > 0 || thirdPartyRewardAprItems.length > 0,
+                hasRewardApr,
             },
         };
     }
