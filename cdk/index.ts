27cf319cd036c2628cc9bbb55d860b4fd8898ae5
Tim Robinson <tim@timjrobinson.com>
2023-03-07 19:26:28 +1000

    Setup one VPC for all and pass it to worker

--- cdk/index.ts
+++ cdk/index.ts
@@ -1,12 +1,19 @@
+require('dotenv').config();
 import { App, Stack, StackProps } from 'aws-cdk-lib'
 import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { Vpc } from 'aws-cdk-lib/aws-ec2';
 import { Worker } from './modules/worker';
 
 export class BeethovenXApi extends Stack {
   constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new Worker(scope, 'Worker');
+    const vpc = new Vpc(this, 'Vpc', { maxAzs: 2 });
+
+    new Worker(scope, 'Worker', {
+      vpc: vpc,
+      databaseUrl: ''
+    });
 
     new Bucket(this, 'BeethovenXTestBucket', {
       versioned: true
--- cdk/modules/worker.ts
+++ cdk/modules/worker.ts
@@ -5,14 +5,26 @@ import { ContainerImage } from 'aws-cdk-lib/aws-ecs';
 import { ApplicationLoadBalancedFargateService } from 'aws-cdk-lib/aws-ecs-patterns';
 import path = require('path');
 
+export interface WorkerProps extends StackProps {
+  /**
+   * VPC Id
+   */
+  vpc: Vpc;
+
+  /**
+   * URL of the Postgres database. Should be in format:
+   * postgresql://USER:PASSWORD@HOST:POST/DB_NAME
+   */
+  databaseUrl: string
+}
+
 export class Worker extends Stack {
-    constructor(scope: App, id: string, props?: StackProps) {
+    constructor(scope: App, id: string, props: WorkerProps) {
       super(scope, id, props);
 
-      // Create VPC and Fargate Cluster
+      // Create Fargate Cluster
       // NOTE: Limit AZs to avoid reaching resource quotas
-      const vpc = new Vpc(this, 'WorkerVpc', { maxAzs: 2 });
-      const cluster = new Cluster(this, 'WorkerCluster', { vpc });
+      const cluster = new Cluster(this, 'WorkerCluster', { vpc: props.vpc });
 
       // Instantiate Fargate Service with a cluster and a local image that gets
       // uploaded to an S3 staging bucket prior to being uploaded to ECR.
@@ -20,11 +32,16 @@ export class Worker extends Stack {
       // with the image from ECR.
       new ApplicationLoadBalancedFargateService(this, 'FargateService', {
           cluster,
+          assignPublicIp: true,
           taskImageOptions: {
               image: ContainerImage.fromAsset(path.resolve(__dirname, '..', '..')),
               environment: {
+                ...process.env,
                 'WORKER': 'true',
-              }
+                'DATABASE_URL': props.databaseUrl, 
+              },
+              containerName: 'Worker',
+              containerPort: 4000,
           },
       });
     }
