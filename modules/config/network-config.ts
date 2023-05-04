b91ebf3bcd98dc8b3a5fe159134b393aa7f48f2b
franz <franzns11@gmail.com>
2023-05-04 09:17:38 +0200

    add erc4626 as reaper linaer

--- modules/config/network-config.ts
+++ modules/config/network-config.ts
@@ -239,7 +239,10 @@ const AllNetworkConfigs: { [chainId: string]: NetworkConfig } = {
             proxyAddress: '0xbC8a71C75ffbd2807c021F4F81a8832392dEF93c',
         },
         reaper: {
-            linearPoolFactories: ['0xd448c4156b8de31e56fdfc071c8d96459bb28119'],
+            linearPoolFactories: [
+                '0xd448c4156b8de31e56fdfc071c8d96459bb28119',
+                '0x89857161e0ad36f8c5a537733c1fcf7145220aae',
+            ],
             averageAPRAcrossLastNHarvests: 5,
         },
         beefy: {
