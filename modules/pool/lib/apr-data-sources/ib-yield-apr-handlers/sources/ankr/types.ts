19a48fcc4f49cbf6cec02974b11424b04eec0b5c
Luiz Gustavo Abou Hatem De Liz <luizgustavo@balancerlabs.dev>
2023-08-01 19:26:35 -0300

    Types files for ankr and default-fetch; (Missing in the last commit)

---
+++ modules/pool/lib/apr-data-sources/ib-yield-apr-handlers/sources/ankr/types.ts
@@ -0,0 +1,5 @@
+export type AnkrAprHandlerConfig = {
+  serviceName: string;
+  tokenAddress: string;
+  network: number;
+}
---
+++ modules/pool/lib/apr-data-sources/ib-yield-apr-handlers/sources/default-fetch/types.ts
@@ -0,0 +1,7 @@
+export type DefaultAprHandlerConfig = {
+  tokens: string[];
+  url: string;
+  network: number;
+  scale?: number;
+  path?: string;
+}
