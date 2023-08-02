4cff5b7305571966f6f964e24f8732486041889e
Luiz Gustavo Abou Hatem De Liz <luizgustavo@balancerlabs.dev>
2023-08-01 21:22:28 -0300

    Removing test route; Adding migration file;

--- modules/balancer/loadRestRoutes.ts
+++ modules/balancer/loadRestRoutes.ts
@@ -6,15 +6,4 @@ import { prisma } from "../../prisma/prisma-client";
 
 export function loadRestRoutesBalancer(app: Express) {
     app.use('/health', (req, res) => res.sendStatus(200));
-    app.use('/test', async (req, res) => {
-        const pools = await prisma.prismaPool.findMany({
-            ...prismaPoolWithExpandedNesting,
-            where: { chain: networkContext.chain },
-        });
-        const ibTokensAprService = new IbTokensAprService(1);
-        await ibTokensAprService.updateAprForPools(pools);
-
-        return res.sendStatus(200)
-    });
-
 }
