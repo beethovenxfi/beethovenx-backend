1e74f53b7e8b896656f5c87403854571578de922
Luiz Gustavo Abou Hatem De Liz <luizgustavo@balancerlabs.dev>
2023-08-07 10:07:08 -0300

    removing unused imports;

--- modules/balancer/loadRestRoutes.ts
+++ modules/balancer/loadRestRoutes.ts
@@ -1,8 +1,4 @@
 import { Express } from 'express';
-import { IbTokensAprService } from "../pool/lib/apr-data-sources/ib-tokens-apr.service";
-import { networkContext } from "../network/network-context.service";
-import { prismaPoolWithExpandedNesting } from "../../prisma/prisma-types";
-import { prisma } from "../../prisma/prisma-client";
 
 export function loadRestRoutesBalancer(app: Express) {
     app.use('/health', (req, res) => res.sendStatus(200));
