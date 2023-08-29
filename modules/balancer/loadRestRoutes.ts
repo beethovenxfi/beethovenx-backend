ae0d54cc91495f9270fba9ae71d254fc185528f2
Luiz Gustavo Abou Hatem De Liz <luizgustavo@balancerlabs.dev>
2023-08-29 19:29:57 -0300

    Removing test route;

--- modules/balancer/loadRestRoutes.ts
+++ modules/balancer/loadRestRoutes.ts
@@ -1,27 +1,5 @@
 import { Express } from 'express';
-import { prisma } from '../../prisma/prisma-client';
-import { prismaPoolWithExpandedNesting } from '../../prisma/prisma-types';
-import { networkContext } from '../network/network-context.service';
-import { IbTokensAprService } from '../pool/lib/apr-data-sources/ib-tokens-apr.service';
-import { tokenService } from '../token/token.service';
-import { mainnetNetworkData } from '../network/mainnet';
-import { optimismNetworkData } from '../network/optimism';
 
 export function loadRestRoutesBalancer(app: Express) {
     app.use('/health', (req, res) => res.sendStatus(200));
-    app.use('/test', async (req, res) => {
-        const pools = await prisma.prismaPool.findMany({
-            ...prismaPoolWithExpandedNesting,
-            where: { chain: networkContext.chain },
-        });
-        const ibTokensAprService = new IbTokensAprService(
-            optimismNetworkData.ibAprConfig,
-            optimismNetworkData.chain.prismaId,
-            optimismNetworkData.chain.id,
-            tokenService,
-        );
-        await ibTokensAprService.updateAprForPools(pools);
-
-        return res.sendStatus(200);
-    });
 }
