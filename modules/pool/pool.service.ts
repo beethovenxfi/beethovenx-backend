94201129ba3363a134c82c409bbcca32c29680b2
gmbronco <83549293+gmbronco@users.noreply.github.com>
2024-01-09 19:09:17 +0100

    obsolete poolReloadPoolTokenIndexes
    covered by syncAllPoolsFromSubgraph

--- modules/pool/pool.service.ts
+++ modules/pool/pool.service.ts
@@ -336,10 +336,6 @@ export class PoolService {
         await this.poolCreatorService.reloadAllTokenNestedPoolIds();
     }
 
-    public async reloadPoolTokenIndexes(poolId: string) {
-        await this.poolCreatorService.reloadPoolTokenIndexes(poolId);
-    }
-
     public async setPoolsWithPreferredGaugesAsIncentivized() {
         await this.poolSyncService.setPoolsWithPreferredGaugesAsIncentivized();
     }
